92f67f890ef200c5a86e6d9c8d355d74c5f0fd3e
rayane866 <54329086+rayane866@users.noreply.github.com>
2019-12-21 17:47:49 +0100

    Add files via upload

--- PearBot.py
+++ PearBot.py
@@ -11,8 +11,14 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print(f"{message.author.name}> {message.content}")
-    if message.content in settings["welcome"]:
-        await message.channel.send("Welcome")
-
+    id = client.get_guild(settings["server_id"])
+    if str(message.author) != str(client.user):
+        print(f"{message.author.name}@{message.channel}> {message.content}")
+        for word in settings["welcome"]:
+            if word in str(message.content).lower():
+                await message.channel.send("Welcome")
+        if str(message.channel) in settings["cmd_channels"]:
+            if str(message.content) == "!users":
+                await message.channel.send(f"Number of users is: {id.member_count}")
+    
 client.run(str(settings["token"]))
